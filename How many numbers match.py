d45378130645d2b7547be743ba0e8625c6e817fe
Gessika <bugtest.fr@gmail.com>
2016-03-14 11:39:57 +0100

    How many numbers match

---
+++ How many numbers match.py
@@ -0,0 +1,18 @@
+# How many numbers match
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == b == c:
+    print '3'
+
+elif a == b or a == c or b == c:
+    print '2'
+
+elif a != b != c:
+    print '0'
+
+
+
+
