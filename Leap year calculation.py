45211ebc48d58bcb1846ac801eec8f336a254988
Gessika <bugtest.fr@gmail.com>
2016-03-14 11:39:29 +0100

    Leap year calculation

---
+++ Leap year calculation.py
@@ -0,0 +1,14 @@
+# calculation of the Leap year
+
+x = int(input())
+
+if x % 4 == 0 and x % 100 == 0:
+    if x % 400 == 0:
+        print 'YES'
+    else:
+        print "NO"
+        
+elif x % 4 != 0:
+    print "NO"
+elif x % 4 == 0:
+    print 'YES'
